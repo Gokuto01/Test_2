2737308ac7d39de48abddd07009ae43fcc08f0ad
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-11-17 15:23:20 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itf13.mysql.database.azure.com', 'Gokuto@itf13', 'Golf1112', 'ITFLAB', 3306);
+mysqli_real_connect($conn, 'itf13.mysql.database.azure.com', 'Gokuto@itf13', 'Golf1112', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
